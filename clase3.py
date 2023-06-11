f57478585520dfa656d8a1ef9fb230f8a82bac5e
alangiercowski <81708985+alangiercowski@users.noreply.github.com>
2023-06-11 16:57:44 -0300

    Add files via upload

---
+++ clase3.py
@@ -0,0 +1,40 @@
+#Asignación de valores
+
+#1
+numero = 1
+#2
+numero2 = 2.5
+#3
+boolean = True
+#4
+numero = 4
+#5
+lista = ["a", "b", "c"]
+
+#Operadores matematicos
+
+#1
+suma = 5+4
+#2
+resta = 5-4
+#3
+division = 20/2
+#4
+multiplicacion = 5*9
+#5
+elevada = 3**8
+#6
+divisionEntera = int(9/4)
+#7
+modulo = 15%2
+
+#Redondeo de floats
+
+papas = 646.21
+pan = round((250.072),2)
+hamburguesas = round((825.472),2)
+gaseosa = round((350.112),2)
+mayonesa = round((541.101),2)
+queso = 332.05
+
+precioTotal = papas + pan + hamburguesas + gaseosa + mayonesa + queso
